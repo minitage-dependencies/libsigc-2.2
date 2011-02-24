b2913ec87a8a5cb72cef4422439ea352f8059623
kiorky <kiorky@cryptelium.net>
2011-02-24 11:40:20 +0100

    up

--- setenv.py
+++ setenv.py
@@ -6,7 +6,6 @@ def h(options,buildout):
         make = which("gmake")
     except:
         make = "make"
-    import pdb;pdb.set_trace()
     os.environ['MAKE'] = make
 
 # vim:set ts=4 sts=4 et  :
